87e3f0ba13a6538bfe0149b76a32f1d547cd5e73
Alper Dur <aalperd98@gmail.com>
2026-09-06 17:38:32 +0300

    Rate limit register and login endpoints

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register'])
+    ->middleware('throttle:10,1');
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->middleware('throttle:6,1');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
